824e2635c665f70ebe688b8996c5927111c5cda5
HS <hasang2691@naver.com>
2024-05-19 02:14:32 +0900

    Refactor: Create ContentEditing and ContentView component

--- src/apis/useContentDeleteMutation.ts
+++ src/apis/useContentDeleteMutation.ts
@@ -1,4 +1,6 @@
 // 미션: 삭제 구현하기(delete 활용)
 // 삭제 버튼을 눌렀을 때 mutation
 // 삭제 버튼을 눌렀을 때 -> 삭제 버튼 누르고 useNavigate 사용해서 board로 이동
-// Content.tsx 136번째 줄 onClick
+// 1. apis/useContentDeleteMutation.ts 구현
+// 2. pages/Content.tsx에서 handleContentDeleteClick 구현
+// 3. organisms/ContentView.tsx도 코드 추가
--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -4,16 +4,14 @@ import { useParams } from 'react-router-dom';
 import DarkModeToggle from '../components/atoms/DarkModeToggle';
 
 import Loading from './Loading';
-import LikesBtn from '../components/molecules/LikesBtn';
 import SearchHeader from '../components/organisms/Appbar';
-import { IconButton } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import BuildIcon from '@mui/icons-material/Build';
-import SendIcon from '@mui/icons-material/Send';
+
 import { useFeedDataQuery } from '../apis/fetchFeedsData';
 
 import useContentEditMutation from '../apis/useContentEditMutation';
 import ScrollToTop from './../components/atoms/ScrollToTop';
+import ContentEditing from '../components/organisms/ContentEditing';
+import ContentView from '../components/organisms/ContentView';
 
 const Content: React.FC = () => {
   // useParams를 통하여 uri에 있는 id를 가져옴
@@ -81,67 +79,25 @@ const Content: React.FC = () => {
       <SearchHeader />
       <div className=" text-black sm:mx-32 pb-72">
         {isEditing ? (
-          // 수정 중이 아닐 때
-          <>
-            <div className="flex my-8">
-              <div className="text-xl font-semibold">제목</div>
-            </div>
-            <input
-              value={editedTitle}
-              onChange={handleTitleChange}
-              className="w-full p-2 border rounded-md h-10 my-4"
-            />
-            <div className="flex my-8">
-              <div className="text-xl font-semibold">본문</div>
-            </div>
-            <textarea
-              value={editedBody}
-              onChange={handleBodyChange}
-              className="w-full h-full p-2 border rounded-md"
-            />
-            <div className="flex justify-end whitespace-nowrap mt-10">
-              <SendIcon
-                sx={{ color: 'black', cursor: 'pointer' }}
-                className="dark:text-white"
-                onClick={handleContentEditClick}
-              />
-            </div>
-          </>
+          // 수정 중일 때
+          <ContentEditing
+            editedTitle={editedTitle}
+            handleTitleChange={handleTitleChange}
+            editedBody={editedBody}
+            handleBodyChange={handleBodyChange}
+            handleContentEditClick={handleContentEditClick}
+          />
         ) : (
           // 수정 중이 아닐 때
-          <>
-            <div className="text-2xl font-bold dark:text-white mt-4">
-              {feedData.title}
-            </div>
-            <div className="text-sm text-gray-400 dark:text-white mt-4">
-              {feedData.author} | {feedData.time}
-            </div>
-            <div className="h-auto bg-gray-100 dark:bg-gray-800 dark:text-white rounded-lg mt-10 p-4 ">
-              {feedData.body}
-            </div>
-            <div className="flex justify-between mt-10">
-              {/* feedData의 likeCount 전달 */}
-              <LikesBtn likeCount={feedData.likeCount} />
-
-              <div className="flex gap-x-2 whitespace-nowrap">
-                {/* 수정 버튼 */}
-                <IconButton aria-label="fix" onClick={handleEditClick}>
-                  <BuildIcon
-                    sx={{ color: 'black' }}
-                    className="dark:text-white"
-                  />
-                </IconButton>
-
-                {/* 삭제 버튼 */}
-                <IconButton aria-label="delete" onClick={() => {}}>
-                  <DeleteIcon
-                    sx={{ color: 'black' }}
-                    className="dark:text-white"
-                  />
-                </IconButton>
-              </div>
-            </div>
-          </>
+          <ContentView
+            title={feedData.title}
+            author={feedData.author}
+            time={feedData.time}
+            body={feedData.body}
+            likeCount={feedData.likeCount}
+            handleEditClick={handleEditClick}
+            // handleContentDeleteClick = {handleContentDeleteClick} <- 미션 구현
+          />
         )}
       </div>
       <DarkModeToggle />
